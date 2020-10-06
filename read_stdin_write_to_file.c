7d1651c45113c06b904fb2c5d70f1c209a41b26c
Rajkumar Radhamanalan <rajkradh@cisco.com>
2020-10-06 23:01:27 +0530

    BugID: 2020/10/06 - Fix code for release
    Why implement this and any critical decision making reasoning
    
    Reviewed-By:

--- read_stdin_write_to_file.c
+++ read_stdin_write_to_file.c
@@ -7,13 +7,13 @@ char mdata_file[512];
 void unit_test_api(off_t fsize)
 {
     char command[100];
-    // For UT verifcation show the copied file
+    // For UT verification show the copied file
     if (fsize) {
-        printf("Given file sie is %ld and find copied file detail below \n", fsize);
-        sprintf(command, "ls -l ip_data_copy %s", mdata_file);
+        printf("Given file size is %ld and find copied file detail below \n", fsize);
+        snprintf(command, sizeof(command), "ls -l ip_data_copy %s", mdata_file);
     } else {
         printf("\n Copied file 'ip_data_copy' contents are:\n");
-        sprintf(command, "ls -l ip_data_copy \n\n cat ip_data_copy");
+        snprintf(command, sizeof(command), "ls -l ip_data_copy \n\n cat ip_data_copy");
     }
     system(command);
 
@@ -28,25 +28,28 @@ int main()
     FILE *fp = NULL;
     FILE *ifp = stdin;
 
+    fp = fopen("ip_data_copy", "wb");
+    if (fp == NULL) {
+        printf("\n ERROR: File 'ip_data_copy' open has failed to copy data,"
+               " errno %d \n", errno);
+        return 0;
+    }
+
     printf("\n Is input image, video or music file(y/N):");
     scanf("%c", &input);
-    // Get file name incase multimedia data
+    // Get file name if input is multimedia data
     if (input == 'y') {
         printf("\n Enter file path (path length should not > 512):");
         scanf("%s", mdata_file);
         ifp = fopen(mdata_file, "rb");
         if (ifp == NULL) {
-            printf("\n ERROR: Given file '%s' is not found, errno %d \n", mdata_file, errno);
+            printf("\n ERROR: Given file '%s' is not found, errno %d \n",
+                    mdata_file, errno);
+            fclose(fp);
             return 0;
         }
     } else {
         printf("\n Enter data (press ctr+d if input over):\n");
-    }
-
-    fp = fopen("ip_data_copy", "wb");
-    if (fp == NULL) {
-        printf("\n ERROR: File 'ip_data_copy' open has failed to copy data, errno %d \n", errno);
-        return 0;
     }
 
     while (bytesread = fread(buffer, 1, 1, ifp)) {

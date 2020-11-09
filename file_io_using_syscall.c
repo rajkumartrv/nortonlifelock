ca7fae238ff91f455b24b64e422848d68024a057
Rajkumar Radhamanalan <rajkradh@cisco.com>
2020-11-09 19:52:36 +0530

    BugID: 2020/11/09-Check memory and CPU error condition
    Why implement this and any critical decision making reasoning
    
    Reviewed-By:

--- file_io_using_syscall.c
+++ file_io_using_syscall.c
@@ -2,11 +2,11 @@
  * In this program, handled arbitrary input data via STDIN,
  * and copied them into file "ip_data_copy".
  *
- * If the input is only text then read them via STDIN otherwise get the file path of
- * binary, image, video, or music file and copy into ip_data_copy.
+ * If the input is only text then read them via STDIN otherwise get the
+ * file path of binary, image, video, or music file and copy into ip_data_copy.
  *
- * Before run the program make sure, login as super user so that avoid any error
- * on create and setting file permission
+ * Before run the program make sure, login as super user so that
+ * avoid any error on create and setting file permission
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,36 +15,45 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <errno.h>
+#include <malloc.h>
 
 /*
  * Desc:
  *  This function copy all other format file like mp3, exe, mp4, jpg, etc
- *  and wrtite into file "ip_data_copy"
+ *  and write into file "ip_data_copy"
  *
  * Arguments:
  *  reader - File descriptor of input file to be copy
  *  writer - File descriptor of new file to copied data into it
- *  len - size of input file
+ *  file_size - size of input file
  */
-void copy_input_file(int reader, int writer, size_t len)
+void copy_input_file(int reader, int writer, size_t file_size)
 {
     size_t bytesread = 0;
     char *buffer = NULL;
-    buffer = (char *)malloc(len);
-    if (buffer == NULL) {
-        printf("\n Memory failure to copy file\n");
+    size_t alloc_size = 0;
+
+    buffer = (char *)malloc(file_size);
+    alloc_size = malloc_usable_size((void *)buffer);
+    if ((buffer == NULL) || (alloc_size < file_size)) {
+        printf("\n Failed: memory required %ld but"
+                " allocated  %ld\n", file_size, alloc_size);
+        if (alloc_size < file_size) {
+            free(buffer);
+        }
         return;
     }
-    while((bytesread = read(reader, buffer, len)) < 0 && (errno == EINTR))
-        ;//retry in case cpu cycle not available
+
+    while((bytesread = read(reader, buffer, file_size)) < 0 && (errno == EINTR))
+        ;//Retry in case of CPU cycle not available
     while((write(writer, buffer, bytesread)) < 0 && (errno == EINTR))
-        ;//retry in case cpu cycle not available
+        ;//Retry in case of CPU cycle not available
     free(buffer);
 }
 
 /*
  * Desc:
- *  This function copy data from STDIN and wrtite into file "ip_data_copy"
+ *  This function copy data from STDIN and write into file "ip_data_copy"
  *
  * Arguments:
  *  reader - File descriptor of input file to be copy
@@ -75,21 +84,20 @@ int main()
     if (writer == -1) {
         printf("\n ERROR: File 'ip_data_copy' open has failed to "
                 "copy input data, errno %d \n", errno);
-        perror("read_stdin_write_to_file");
+        perror("open");
         return 0;
     }
 
     printf("\n Is input data binary, image, video, or music file(y/N):");
     scanf("%c", &input);
     if (input == 'y') {
-        // Get file path of input data
         printf("\n Enter file path (path length should not > 512):");
         scanf("%s", input_file);
         reader = open(input_file, O_RDONLY);
         if (reader == -1) {
             printf("\n ERROR: Given input file '%s' is not found, "
                     "errno %d \n", input_file, errno);
-            perror("read_stdin_write_to_file");
+            perror("open");
             // Close already opened descriptor
             close(writer);
             return 0;
@@ -110,7 +118,7 @@ int main()
         stat("ip_data_copy", &stat_wr);
         printf("\nSize of input file %s is %ld ", input_file, stat_rd.st_size);
         printf("\nFile copy was successful, with %ld byte copied\n", stat_wr.st_size);
-        snprintf(command, sizeof(command), "ls -l ip_data_copy %s", input_file);
+        snprintf(command, sizeof(command), "ls -lh ip_data_copy %s", input_file);
     } else {
         printf("\n Copied file 'ip_data_copy' contents are:\n");
         snprintf(command, sizeof(command), "ls -l ip_data_copy \n\n cat ip_data_copy");
